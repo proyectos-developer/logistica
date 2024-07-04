0f4c5c8f4d00bb2c19424a9752af61651de6f071
proyectos-developer <proyectos@developer-ideas.com>
2024-07-04 10:19:45 -0500

    V1.0.0

--- app.js
+++ app.js
@@ -22,6 +22,41 @@ app.get ('/sobre-nosotros', (req, res) => {
     res.sendFile (path.resolve(__dirname, './client/build/sobre-nosotros', 'index'))
 })
 
+app.use(express.static(path.resolve(__dirname, './client/build/nuestros-servicios')))
+app.get ('/nuestros-servicios', (req, res) => {
+    res.sendFile (path.resolve(__dirname, './client/build/nuestros-servicios', 'index'))
+})
+
+app.use(express.static(path.resolve(__dirname, './client/build/nuestros-servicios')))
+app.get ('/nuestros-servicios/:servicio', (req, res) => {
+    res.sendFile (path.resolve(__dirname, './client/build/nuestros-servicios', 'index'))
+})
+
+app.use(express.static(path.resolve(__dirname, './client/build/noticias')))
+app.get ('/noticias', (req, res) => {
+    res.sendFile (path.resolve(__dirname, './client/build/noticias', 'index'))
+})
+
+app.use(express.static(path.resolve(__dirname, './client/build/noticias')))
+app.get ('/noticias/:id_notica', (req, res) => {
+    res.sendFile (path.resolve(__dirname, './client/build/noticias', 'index'))
+})
+
+app.use(express.static(path.resolve(__dirname, './client/build/galeria')))
+app.get ('/galeria', (req, res) => {
+    res.sendFile (path.resolve(__dirname, './client/build/galeria', 'index'))
+})
+
+app.use(express.static(path.resolve(__dirname, './client/build/cotizacion')))
+app.get ('/cotizacion', (req, res) => {
+    res.sendFile (path.resolve(__dirname, './client/build/cotizacion', 'index'))
+})
+
+app.use(express.static(path.resolve(__dirname, './client/build/contactanos')))
+app.get ('/contactanos', (req, res) => {
+    res.sendFile (path.resolve(__dirname, './client/build/contactanos', 'index'))
+})
+
 //Iniciar el servidor
 app.listen (app.get('port'), () => {
     console.log ('Server en puerto ', app.get ('port'))
